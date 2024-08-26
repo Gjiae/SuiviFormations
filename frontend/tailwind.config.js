20001b4aa54c767a1d061a9e835269c28d4713b4
Valentin Gazzoli <valentin.gazzoli@orange.fr>
2024-08-26 21:09:41 +0200

    tailles police

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],  theme: {
         fontSize: {
-          '8xl': ['24px', {
+          '8xl': ['26px', {
             lineHeight: '33px',
             letterSpacing: '0%',
             fontWeight: 'Bold',
+          }],
+          '7xl': ['16px', {
+            lineHeight: '24px',
+            letterSpacing: '0px',
+            fontWeight: 'Semi Bold',
+          }],
+          '6xl': ['14px', {
+            lineHeight: '22px',
+            letterSpacing: '0px',
+            fontWeight: 'Medium',
           }],
         },
     extend: {},

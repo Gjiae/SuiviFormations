67bde32ae0055af0b36c05f75d11ed863c2bcdde
Gjiae <valentin.gazzoli@orange.fr>
2024-08-30 14:40:54 +0200

    Ajout d'un layout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { Navigation } from '@/ui/components/navigation/navigation'
 import { Footer } from '@/ui/components/navigation/footer'
+import { Layout } from '@/ui/components/layout/layout'
+import { Seo } from '@/ui/components/seo'
 
 export default function Home() {
   return (
     <>
-      <Navigation />
-      <Footer />
+      <Seo
+        title="Page d'accueil de l'application web de suivi des formations"
+        description="Description..."
+      />
+      <Layout>Hello World</Layout>
     </>
   )
 }

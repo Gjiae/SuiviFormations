df86d62051297da2f7e3a071aea2ddd1d7f601cd
Gjiae <valentin.gazzoli@orange.fr>
2024-08-30 09:58:00 +0200

    Ajout d'un logo

--- frontend/src/ui/design-system/typography.tsx
+++ frontend/src/ui/design-system/typography.tsx
@@ -1,58 +1,65 @@
-import clsx from "clsx";
+import clsx from 'clsx'
 
 interface Props {
-    variant?: "h1" | "h2" | "h3" | "h4" | "h5" | "Lead" | "body-lg" | "body-base" | "body-sm" | "caption1" | "caption2" | "caption3" | "caption4";
-    component?: "h1" | "h2" | "h3" | "h4" | "h5" | "div" | "p" | "span";
-    theme?: "black" | "grey" | "white" | "primary" | "secondary";
-    weight?: "regular" | "medium";
-    className?: string;
-    children: React.ReactNode;
+  variant?: 'h1' | 'h2' | 'h3'
+  component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'div' | 'p' | 'span'
+  theme?: 'black' | 'gray' | 'white' | 'primary' | 'secondary'
+  weight?: 'regular' | 'medium'
+  className?: string
+  children: React.ReactNode
 }
 
 export const Typography = ({
-    variant = "h3",
-    component: Component = "div",
-    theme = "black",
-    weight = "regular",
-    className,
-    children
-}:Props) => {
-    
-    let variantStyles: string = "", colorStyles: string;
+  variant = 'h3',
+  component: Component = 'div',
+  theme = 'black',
+  weight = 'regular',
+  className,
+  children,
+}: Props) => {
+  let variantStyles: string = '',
+    colorStyles: string = ''
 
-    switch (variant) {
-        case "h1":
-            variantStyles = "text-H4"
-            break;
-        case "h2":
-            variantStyles = "text-H5"
-            break;
-        case "h3":
-            variantStyles = "text-H6"
-            break;
-    }
+  switch (variant) {
+    case 'h1':
+      variantStyles = 'text-H4'
+      break
+    case 'h2':
+      variantStyles = 'text-H5'
+      break
+    case 'h3':
+      variantStyles = 'text-H6'
+      break
+  }
 
-    switch (theme) {
-        case "white":
-            colorStyles = "text-white";
-            break;
-        case "primary":
-            colorStyles = "text-primary";
-            break;
-        case "secondary":
-            colorStyles = "text-secondary";
-            break;
-        }
+  switch (theme) {
+    case 'black':
+      colorStyles = 'text-dark'
+      break
+    case 'white':
+      colorStyles = 'text-white'
+      break
+    case 'gray':
+      colorStyles = 'text-textcolor'
+      break
+    case 'primary':
+      colorStyles = 'text-primary'
+      break
+    case 'secondary':
+      colorStyles = 'text-secondary'
+      break
+  }
 
-
-    return (
-        <Component 
-            className={clsx(
-                variantStyles,
-                weight === "medium" && "font-medium",
-                className,
-            )}>
-            {children}
-        </Component>
-    )
+  return (
+    <Component
+      className={clsx(
+        variantStyles,
+        weight === 'medium' && 'font-medium',
+        className,
+        colorStyles
+      )}
+    >
+      {children}
+    </Component>
+  )
 }

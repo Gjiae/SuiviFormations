f993bc3aad3bb5a9ad1d7bb4f21a3e2ad52bc8ab
Gjiae <valentin.gazzoli@orange.fr>
2024-09-09 10:48:16 +0200

    breadcrumbs : affichage et liens des pages dans la navigation

---
+++ frontend/src/ui/components/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,44 @@
+import { Typography } from '@/ui/design-system/typography'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { v4 as uuidv4 } from 'uuid'
+
+export const Breadcrumbs = () => {
+  const router = useRouter()
+  const asPath = router.asPath
+  const segments = asPath.split('/')
+  const lastSegment = segments[segments.length - 1]
+  segments[0] = 'Tableau de bord'
+
+  const view = segments.map((path, index) => (
+    <div key={uuidv4()}>
+      <Link
+        href={index > 0 ? `/${segments.slice(1, index + 1).join('/')}` : '/'}
+      >
+        <Typography
+          variant="14Med"
+          component="span"
+          weight="medium"
+          className={clsx(
+            path !== lastSegment ? 'text-textcolor' : 'text-dark',
+            'transition-all hover:text-primary hover:opacity-100'
+          )}
+        >
+          {path}
+        </Typography>
+      </Link>
+      {path !== lastSegment && (
+        <Typography
+          variant="14Med"
+          component="span"
+          className="ml-2 text-textcolor"
+        >
+          /
+        </Typography>
+      )}
+    </div>
+  ))
+
+  return <>{view}</>
+}
--- frontend/src/ui/components/navigation/navigation.tsx
+++ frontend/src/ui/components/navigation/navigation.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link'
 import { Button } from '../../design-system/button'
-import { Typography } from '../../design-system/typography'
 import { Container } from '../container'
 import { FaBell } from 'react-icons/fa6'
+import { Breadcrumbs } from '../breadcrumbs/breadcrumbs'
 
 interface Props {}
 
@@ -11,34 +10,7 @@ export const Navigation = ({}: Props) => {
     <div className="flex flex-col-reverse justify-between gap-6 mt-5 md:flex-row md:items-center">
       <Container className="flex items-center justify-between py-3 gap-7">
         <div className="flex items-center gap-2.5">
-          <Link href="/">
-            <Typography
-              variant="14Med"
-              theme="gray"
-              weight="medium"
-              className="transition-all hover:text-primary hover:opacity-100"
-            >
-              Tableau de bord
-            </Typography>
-          </Link>
-          <Typography
-            variant="14Med"
-            theme="black"
-            weight="medium"
-            className="transition-all hover:text-primary hover:opacity-100"
-          >
-            /
-          </Typography>
-          <Link href="/">
-            <Typography
-              variant="14Med"
-              theme="black"
-              weight="medium"
-              className="transition-all hover:text-primary hover:opacity-100"
-            >
-              Test
-            </Typography>
-          </Link>
+          <Breadcrumbs />
         </div>
 
         <div className="flex items-right gap-7">
